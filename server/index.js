c9c73fe33981042a4d9c37d624ef3477114161b5
rain46688 <rain46688@gmail.com>
2021-04-15 20:38:26 +0900

    이젠 된다.

---
+++ server/index.js
@@ -0,0 +1,132 @@
+const path = require('path');
+const express = require('express')
+const app = express()
+//const config = require('./config/key')
+const { Member } = require('./model/Member')
+//이렇게 써도 무관
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+const monoose = require('mongoose')
+//cookieparser사용하기 토큰 쿠키에 저장
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+//미들웨어 auth
+const { auth } = require('./middleware/auth')
+
+monoose.connect('mongodb+srv://circlestar:alstn8775*@cmsmongo.07d76.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('몽고 디비 연결됨!'))
+  .catch(err => console.log(err))
+
+// 미들웨어 함수를 특정 경로에 등록
+app.use('/api/data', function(req, res) {
+  res.json({ greeting: 'Hello cms World' });
+});
+
+//회원 추가하기
+app.post('/api/users/register', (req, res) => {
+  //회원 가입 필요한 정보들을 client에서 가져오면 그것을 DB에 넣어준다.
+  const user = new Member(req.body)
+  //req.body는 json 형식으로 넘어온다.
+  //이건 bodyParser를 이용해서 클라이언트의 정보를 req로 받아주는것이라고한다.
+
+  //몽고 디비에있는 함수
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    //성공 못하면 false랑 err 내용이 뜨는것
+    return res.status(200).json({//200은 성공을 의미
+      success: true
+      //성공하면 success가 true가 뜨고
+    })
+  })
+})
+
+//로그인하기
+app.post('/api/users/login', (req, res) => {
+
+  //이메일 디비에서 확인
+  //몽고디비에서 제공하는 findOne 함수를 이용해서 찾기
+  //req.body.email는 넘어오는 리퀘스트에 email 값
+  Member.findOne({ email: req.body.email }, (err, userInfo) => {
+    //userInfo가 없으면 로그인 실패라고 표시해줌
+    console.log("userInfo : " + userInfo)
+    //userInfo 값에는 몽고디비에서 해당 이메일에 맞는 값들을 가져와줌 role,id,name,email 등등
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "로그인 실패"
+      })
+    }
+    //비밀번호 확인
+    //comparePassword 함수는 Users.js에서 만듬
+    userInfo.comparePassword(req.body.password, (err, isMatch) => {
+      console.log("isMatch : " + isMatch)
+      console.log("err : " + err)
+      console.log("req.body.password : " + req.body.password)
+
+      //isMatch가 false인경우
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호 틀림"
+        })
+      //isMatch가 true인 경우
+      //로그인 성공하면 토큰 생성하기
+      userInfo.generateToken((err, userInfo) => {
+        //err가 있으면 400 페이지랑 에러를 표시한다
+        console.log("err : " + err)
+        if (err) return res.status(400).send(err)
+
+        //토큰을 쿠키나 로컬스토리지에 저장한다
+        res.cookie("wowloginMember", userInfo.token).status(200).json({
+          loginSuccess: true,
+          userId: userInfo._id,
+          message: "로그인 성공"
+        })
+      })
+    })
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  console.log("req.user._id : " + req.user._id)
+  //몽고 디비에서 제공하는 함수
+  Member.findOneAndDelete({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).send({
+      success: true,
+      message: "로그 아웃 성공"
+    })
+  })
+})
+
+//auth 미들웨어이다.
+app.get('/api/users/auth', auth, (req, res) => {
+  console.log("미들웨어 지나서 메소드 실행됨");
+  //여기까지 미들웨어를 통과해왔다는것은 
+  //authentication 이 true라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAuth: true,
+    memberId: req.user.memberId,
+    token: req.user.token
+  })
+
+})
+
+
+// --> Add this
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, (req, res) => {
+    console.log(`server listening on port: ${PORT}`)
+  });
---
+++ server/model/Member.js
@@ -0,0 +1,102 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+// 10글자의 솔트값을 만든다는 의미
+const saltRounds = 10
+//jsonwebtoken 가져오기
+const jwt = require('jsonwebtoken');
+
+
+//스키마 생성하기
+const userSchma = mongoose.Schema({
+    memberId: {
+        type: String,
+        trim: true,
+        unique: 1
+    }, password: {
+        type: String,
+        maxlength: 200
+    },
+    token: {
+        type: String
+    }
+})
+
+//save하기 전에 즉 몽고 디비에 넣기 전에 처리할 함수를 적는것
+//비밀번호를 암호화하려면 디비에 넣기 전에 해야되기때문이다.
+userSchma.pre('save', function (next) {
+    //비밀번호를 암호화 시킨다.
+    let user = this;//이건 위에 모델들을 가르키는것이다. user.password하면 사용자가 입력한 비밀번호가 넘어옴
+    if (user.isModified('password')) {
+        //패스워드가 변경될때만 실행되게 만듬
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            //에러 났을 경우
+            if (err) return next(err)
+            //user.password 플래인 패스워드
+            //성공했을경우
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                //에러 났을경우
+                if (err) return next(err)
+                //성공했을 경우
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchma.methods.comparePassword = function (plainPassword, callbackFunction) {
+    console.log("Users.js comparePassword실행")
+    //플레인 패스워드를 암호화해서 디비에 비교해서 같은지 확인
+    console.log("plainPassword : " + plainPassword)
+    console.log("this.password : " + this.password)
+    bcrypt.compare(plainPassword, this.password, function (err, isMatchPassword) {
+        //bcrypt의 compare함수로 플레인 패스워드를 암호화하고 기존 패스워드랑 비교
+        if (err) return callbackFunction(err)
+        //callbackFunction 콜백 함수로 err를 보내거나 null, isMatchPassword(참거짓)를 일치하냐 안하냐 보냄
+        callbackFunction(null, isMatchPassword)
+    })
+}
+
+userSchma.methods.generateToken = function (callbackFunction) {
+    //jsonwebToken을 이용해서 token을 생성하기
+    //위의 모델에서 this로 가져오는것 
+    const user = this
+    //_id는 몽고 디비에 자동으로 들어가는 id 컬럼, 유저마다 식별자, 기본키 라보면됨
+    let token = jwt.sign(user._id.toHexString(), 'cmsweb')
+    //user._id, 'cmsweb' 둘을 합쳐서 token을 만드는것이고 'cmsweb'으로 user id를 가져올수있음
+    user.token = token
+    //몽고 디비에서 쓰는 save함수를 이용해서 디비에 token값 넣기
+    user.save(function (err, user) {
+        if (err) return callbackFunction(err)
+        //에러가 없는 경우에는 에러는 null이고 user정보를 넣어줌
+        callbackFunction(null, user)
+    })
+}
+
+
+//token은 auth.js에서 넘어온 쿠키에 있던 토큰값 현재 로그인한 유저 토큰
+userSchma.statics.findByToken = function (token, callbackFunction) {
+    console.log("findByToken 메소드 실행됨");
+    let user = this;
+
+    //토큰을 디코드하기
+    jwt.verify(token, 'cmsweb', function (err, decode_user_id) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 token과 db에 보관된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decode_user_id, "token": token }, function (err, userInfo) {
+            //근데 이미 로그인해서 토큰이 쿠키에 저장되어있으면
+            //디비랑 비교해서 이미 인증이 된거 아닌가??
+            //로그인할땐 이메일이랑 비밀번호를 비교함
+            if (err) return callbackFunction(err)
+            callbackFunction(null, userInfo)
+        })
+
+
+    })
+}
+
+//모델 생성하기
+const Member = mongoose.model('Member', userSchma)
+module.exports = { Member }
