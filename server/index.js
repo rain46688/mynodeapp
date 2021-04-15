fe061d69da8c14dc9fa6a6a24a978e5bb2f2f4ae
rain46688 <rain46688@gmail.com>
2021-04-15 20:57:04 +0900

    이젠 된다ㅜㅜㅡㅡㅡ

--- server/index.js
+++ server/index.js
@@ -119,11 +119,15 @@ app.get('/api/users/auth', auth, (req, res) => {
 // --> Add this
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+ // app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // app.get('*', function(req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
+  app.use(express.static("client/build"));
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "../client","build","index.html"));
+  })
 }
 
 const PORT = process.env.PORT || 5000
