ea038c7125eeb217a8e29aa71c03bd645f52e2f6
rain46688 <rain46688@gmail.com>
2021-04-15 21:00:16 +0900

    이젠 된다!!!!!

--- server/index.js
+++ server/index.js
@@ -118,12 +118,6 @@ app.get('/api/users/auth', auth, (req, res) => {
 
 // --> Add this
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
- // app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  // app.get('*', function(req, res) {
-  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  // });
   app.use(express.static("client/build"));
   app.get("*", (req,res) => {
     res.sendFile(path.resolve(__dirname, "../client","build","index.html"));
